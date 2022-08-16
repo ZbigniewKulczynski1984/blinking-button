7abeb706da740222194ab9f5ccde4713abb63195
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-08-16 12:26:50 +0200

    info added to main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+const btn = document.querySelector('.btn');
+
+
+const btnAnimation = e => {
+    const top = e.clientY
+    const left = e.clientX
+    //pozycja w którą klikamy
+
+}
+
+btn.addEventListener('click', btnAnimation);
