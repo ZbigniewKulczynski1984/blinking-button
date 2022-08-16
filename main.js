9d5fc885261218b36d714078d826967c1762a276
Zbigniew Kulczński <z-kulczynski@o2.pl>
2022-08-16 12:31:17 +0200

    info added to main.js

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ const btnAnimation = e => {
     const top = e.clientY
     const left = e.clientX
     //pozycja w którą klikamy
-
+    const btnTopPosition = e.target.offsetTop
+    const btnLeftPosition = e.target.offsetLeft
+    //zmienne reprezentują pozycje przycisku
 }
 
 btn.addEventListener('click', btnAnimation);
